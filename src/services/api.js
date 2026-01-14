10094a2286afbb2087dcc93741833ffb444d9bb8
Shubham Pawar <sp8983204544@gmail.com>
2026-01-14 22:33:14 +0530

    updated api details

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://localhost:7226/api";
-
+//const API_BASE_URL = "https://localhost:7226/api";
+const API_BASE_URL = "https://milkbilling-api.deltalogapp.online/api";
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
